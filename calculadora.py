64092f689817f658c80515d26ab64232c30dd89a
FllavioAndrade <flavio.a.s@live.com>
2024-09-10 18:29:35 -0300

    corrigindo a função soma

--- calculadora.py
+++ calculadora.py
@@ -5,17 +5,7 @@ app = Flask(__name__)
 def soma(num1, num2):
     return num1 + num2
 
-def subtracao(num1, num2):
-    return num1 - num2
-
-def divisao(num1, num2):
-    if num2 != 0:
-        return num1 / num2
-    else:
-        return "Erro: Divisão por zero!"
-
-def multiplicacao(num1, num2):
-    return num1 * num2
+    
 
 @app.route('/', methods=['GET', 'POST'])
 def calculadora():
